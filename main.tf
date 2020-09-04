fd90b8381be2609775dc2ae47a890e8d585f8446
Kumar Raja <kkumarra@cisco.com>
2020-09-04 20:22:28 +0530

    added

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 resource "aws_key_pair" "main" {
     key_name      = "publickey"
-  public_key      = "/home/ubuntu/.ssh/id_rsa.pub"
+  public_key      = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6W8MoWCGE9eXExKcpCELAON4esfJVzVT2aJFp3dviMwqPO/B1JxBmC1vLCv0NpfeOtQFMVySp6mFjMw3A9Pihca/UqT6SedKtpXiTn2wlEnW2dMCh0SeT+TielCGE3CCw+ogsYZ7iWz7xHZKntall/8tjA/mowaiLvT0KN9e6ObJpQw4fwgm/d/jjn5coy+QRvzsl6rvfyyHhPew4i7XBqzMGoCQLeUks7pqvaqZKD/unPfhgk+uVk7tFn6cmhlZ7n+mrBxDRSEZdKkTP+MBdA6H6/b+6V05i7p38RPYgxCcz85x80WpYvMMcPF5v/c3nfzg0dVlzPAelrZqHq5Xl ubuntu@ip-172-31-46-232"
 
 }
 resource "aws_instance" "k8Master"{
